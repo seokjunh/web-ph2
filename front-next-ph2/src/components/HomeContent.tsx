b8ad5bfa642307f03e0bd4a599aebf9dfc1a1a54
seokjun <seokjunh97@gmail.com>
2025-03-15 11:53:02 +0900

    change crlf to lf

--- front-next-ph2/src/components/HomeContent.tsx
+++ front-next-ph2/src/components/HomeContent.tsx
@@ -5,28 +5,51 @@ import HomeItem2 from "@/components/HomeItem2";
 import { useEffect, useRef } from "react";
 
 const HomeContent = () => {
-  const wheelRef = useRef(null);
-
-  const wheelHandler = (e: React.WheelEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const { deltaY } = e;
-    const { scrollTop } = wheelRef.current;
-    const pageHeight = window.innerHeight;
-
-    if (deltaY > 0) {
-      console.log(deltaY, scrollTop, pageHeight);
-    } else {
-      console.log(deltaY, scrollTop, pageHeight);
-    }
-  };
+  const wheelRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    window.addEventListener("wheel", wheelHandler);
+    const wheelHandler = (e: WheelEvent) => {
+      if (wheelRef.current == null) {
+        return;
+      }
+      const { deltaY } = e;
+      const { scrollTop } = wheelRef.current;
+      const pageHeight = window.innerHeight;
+
+      if (deltaY > 0) {
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          e.preventDefault();
+          //현재 1페이지
+          console.log("현재 1페이지, down");
+          wheelRef.current.scrollTo({
+            top: pageHeight,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          console.log("현재 2페이지, down");
+          wheelRef.current.scrollTo({
+            top: pageHeight * 2,
+            left: 0,
+            behavior: "smooth",
+          });
+        }
+      } else {
+        console.log(deltaY, scrollTop, pageHeight);
+      }
+    };
+
+    const wheelRefCurrent = wheelRef.current;
+    if (wheelRefCurrent == null) {
+      return;
+    }
+    wheelRefCurrent.addEventListener("wheel", wheelHandler);
 
     return () => {
-      window.removeEventListener("whell", wheelHandler);
+      wheelRefCurrent.removeEventListener("wheel", wheelHandler);
     };
-  });
+  }, []);
   return (
     <div ref={wheelRef}>
       <HomeItem1 />
