fe5b3819fd8001482c2f4d507b10cacad39e600e
seokjunh <seokjunh97@gmail.com>
2025-04-02 16:58:35 +0900

    Comment out test file

--- back-springboot-ph2/src/test/java/com/backend/back_springboot_ph2/BackSpringbootPh2ApplicationTests.java
+++ back-springboot-ph2/src/test/java/com/backend/back_springboot_ph2/BackSpringbootPh2ApplicationTests.java
@@ -1,9 +1,9 @@
 package com.backend.back_springboot_ph2;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BackSpringbootPh2ApplicationTests {
 
 	@Test
