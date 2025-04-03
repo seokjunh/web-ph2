5eabb95407abdf77c83018107ea7df447e46e33d
seokjunh <seokjunh97@gmail.com>
2025-04-03 16:34:25 +0900

    update api url

--- front-next-ph2/src/lib/api.ts
+++ front-next-ph2/src/lib/api.ts
@@ -1,8 +1,8 @@
 export async function fetchNotice(page: string) {
   const response = await fetch(
-    `http://localhost:8080/api/notice/list?page=${page}`,
+    `http://ph2-backend:8080/api/notice/list?page=${page}`,
   );
   const data = await response.json();
-  console.log(data);
+
   return data;
 }
